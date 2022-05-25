60abba56b04ec6e1065a688aef805b6eb2bab451
Samuel Quintero Agudelo <squinteroagudelo18@gmail.com>
2022-05-25 00:22:47 -0500

    Implement CommandLine Runner

---
+++ src/main/java/co/com/ias/springboot/reactor/app/SpringbootReactorApplication.java
@@ -0,0 +1,18 @@
+package co.com.ias.springboot.reactor.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringbootReactorApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
+}
---
+++ src/test/java/co/com/ias/springboot/reactor/app/SpringbootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ias.springboot.reactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
