54e4e7bb884e3e088e8a1d7349176add292e3055
Samuel Quintero Agudelo <squinteroagudelo18@gmail.com>
2022-05-25 01:47:22 -0500

    Simple Subscribe Event

--- src/main/java/co/com/ias/springboot/reactor/app/SpringbootReactorApplication.java
+++ src/main/java/co/com/ias/springboot/reactor/app/SpringbootReactorApplication.java
@@ -1,5 +1,7 @@
 package co.com.ias.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +10,24 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringbootReactorApplication implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootReactorApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> names = Flux.just("Sam", "Rick", "Andrew", "Tom", "Steve")
-                .doOnNext(System.out::println);
+        Flux<String> names = Flux.just("Sam", "Rick", "Andrew", "", "Tom", "Steve", "Bob")
+                .doOnNext(name -> {
+                    if (name.isEmpty()) throw new RuntimeException("Name is empty");
+                    System.out.println(name);
+                });
 
-        names.subscribe();
+        names
+                .sort()
+                .subscribe(log::info,
+                        error -> log.error(error.getMessage())
+                );
     }
 }
